526d283c28b048d9df2da0ccf3452907ba0fc087
Debora <deboravolc2@gmail.com>
2025-03-05 20:52:43 -0300

    iniciar o repositorio

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import './navbar.scss';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1 className="page-title">Filmes</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
